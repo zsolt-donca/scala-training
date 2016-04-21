213bc4c874baeab7cfc0a9b8c4f169dab1f8ade3
Zsolt Donca <zdonca@artisaninfrastructure.com>
2016-04-21 16:42:06 +0300

    Example with higher-order function.

---
+++ src/main/scala/com/scalatraining/session02/functions/FunExample03.scala
@@ -0,0 +1,27 @@
+package com.scalatraining.session02.functions
+
+object FunExample03 extends App {
+
+  def abs(n: Int): Int = {
+    if (n < 0) -n
+    else n
+  }
+
+  def factorial(n: Int): Int = {
+    // tail recursion
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc else go(n - 1, n * acc)
+    }
+
+    go(n, 1)
+  }
+
+  // higher-order function (a function that takes another function as parameter)
+  def formatResult(name: String, n: Int, f: Int => Int): String = {
+    s"The $name of $n is ${f(n)}"
+  }
+
+  println(formatResult("absolute value", -42, abs))
+  println(formatResult("factorial", 7, factorial))
+
+}
