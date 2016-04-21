9c8ab90605fe0b500ff1cca6f473ec0357e65c4c
Zsolt Donca <zdonca@artisaninfrastructure.com>
2016-04-21 16:53:39 +0300

    Exercise with polymorphic functions.

--- src/main/scala/com/scalatraining/session02/functions/FunExample05.scala
+++ src/main/scala/com/scalatraining/session02/functions/FunExample05.scala
@@ -4,6 +4,7 @@ import scala.annotation.tailrec
 
 object FunExample05 extends App {
 
+  // polymorphic function ("parametric polymorphism", not to be confused with to OOP "subtype polymorphism")
   def findFirst[T](strings: Array[T], p: T => Boolean): Int = {
     @tailrec
     def loop(n: Int): Int = {
@@ -16,7 +17,14 @@ object FunExample05 extends App {
   }
 
   val names = Array("alice", "bob", "joe")
+
   println(findFirst(names, (name: String) => name == "bob"))
 
   println(findFirst(names, (name: String) => name.startsWith("b")))
 }
+
+
+// TODO Exercise: Write a function that checks whether an array is sorted according to a given comparison function
+//
+// def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean
+//
