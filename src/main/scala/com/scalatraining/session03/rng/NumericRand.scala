1f9565055e28962d14f8d4b45f3943c0c1d172e6
Zsolt Donca <zdonca@artisaninfrastructure.com>
2016-05-02 10:10:18 +0300

    Refactored the code into two.

---
+++ src/main/scala/com/scalatraining/session03/rng/NumericRand.scala
@@ -0,0 +1,40 @@
+package com.scalatraining.session03.rng
+
+object NumericRand {
+
+  val int: Rand[Int] = _.nextInt
+
+  val nonNegativeInt: Rand[Int] = {
+    (rng: RNG) => {
+      val (int, rng2) = rng.nextInt
+      if (int == Int.MinValue) nonNegativeInt(rng2)
+      else (Math.abs(int), rng2)
+    }
+  }
+
+  val nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+  val double: Rand[Double] = map(nonNegativeInt)(_.toDouble / Int.MaxValue)
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    rng: RNG => {
+      val (i, rng2) = nonNegativeInt(rng)
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0)
+        (mod, rng2)
+      else nonNegativeLessThan(n)(rng)
+    }
+  }
+
+  def lessThan(n: Int): Rand[Int] = {
+    flatMap(int)(i => {
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(i)
+      else lessThan(n)
+    })
+  }
+
+  def list[A](n: Int, rand: Rand[A]): Rand[List[A]] = {
+    sequence(List.fill(n)(rand))
+  }
+}
---
+++ src/main/scala/com/scalatraining/session03/rng/package.scala
@@ -0,0 +1,43 @@
+package com.scalatraining.session03
+
+package object rng {
+
+  trait RNG {
+    def nextInt: (Int, RNG)
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (a, rng2) = f(rng)
+      g(a)(rng2)
+    }
+  }
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs match {
+      case head :: tail => map2(head, sequence(tail))(_ :: _)
+      case Nil => unit(Nil)
+    }
+  }
+
+  case class SimpleRNG(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+}
